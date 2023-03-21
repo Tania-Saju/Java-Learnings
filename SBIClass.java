bf56f53640bd4887d3588bbd378446ad8473a021
Tania Saju <76559076+Tania-Saju@users.noreply.github.com>
2023-03-21 17:38:01 +1100

    Add files via upload

---
+++ SBIClass.java
@@ -0,0 +1,21 @@
+package org.jsp.bank.application;
+
+public class SBIClass extends BankClass {
+	double bal = 15000.00;
+
+	public void disp() {
+		System.out.println("Balance =   " + bal);
+	}
+
+	public void withdraw(double x) {
+		System.out.println("withdrawn amt = " + x);
+		bal = bal - x;
+		System.out.println("balance amt = " + bal);
+
+	}
+
+	public void deposit(double y) {
+
+		System.out.println("deposit amt = " + y);
+	}
+}
